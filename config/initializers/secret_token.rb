87699673133e4fac59a7a6fe5812601f92788bc3
d34th4ck3r <gautam1237@gmail.com>
2013-05-20 02:52:46 +0530

    Created the basic with Email Sign Up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BasicLogin::Application.config.secret_token = '37334c755c73637d6c2f2385f339303df5a5820c940a75aa143626fe0070d73465ab5ed20187935ebc717e5f27ed9e3064256b68bc0d803c9657560a7e52dcb3'
